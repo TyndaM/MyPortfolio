06716dc4a8d88d9f6a6bc32d634bbc758091fb1d
MT_Tynda <tindameshoullam@gmail.com>
2025-07-05 22:48:11 +0100

    barre de progression

--- pages/script.js
+++ pages/script.js
@@ -1,8 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const btnMenu = document.querySelector('.btn-menu');
-    const navbar = document.querySelector('.navbar');
+// document.addEventListener('DOMContentLoaded', function () {
+//     const btnMenu = document.querySelector('.btn-menu');
+//     const navbar = document.querySelector('.navbar');
 
-    btnMenu.addEventListener('click', function () {
-        navbar.classList.toggle('open');
+//     btnMenu.addEventListener('click', function () {
+//         navbar.classList.toggle('open');
+//     });
+// });
+
+<script>
+  window.addEventListener("load", () => {
+    const progressBars = document.querySelectorAll(".progress");
+    progressBars.forEach(bar => {
+      const value = bar.getAttribute("data-skill");
+      bar.style.width = value;
     });
-});
+  });
+</script>
